16df762a7bf3bf6f842ef9f2fa72f219d8452baa
FarisHijazi <theefaris@gmail.com>
2022-10-18 21:25:11 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read().replace("](", "](https://raw.githubusercontent.com/FarisHijazi/deblack/master/")
+
+
+setuptools.setup(
+    name="deblack",
+    version="0.0.1",
+    description="Script to remove black frames from a video",
+    long_description=long_description,
+    url="https://github.com/FarisHijazi/deblack",
+    author="Faris Hijazi",
+    author_email="theefaris@gmail.com",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "ffmpeg-python",
+    ],
+    keywords="html table htmltable html-table base64 report",
+    entry_points={
+        "console_scripts": [
+            "deblack=deblack:main",
+        ]
+    },
+)
